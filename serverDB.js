580856994dc424c12f6acb0fef0a6793332ab718
juan <jcarlosjaramillo@yahoo.com>
2025-03-23 21:44:22 -0400

    commit 3

--- serverDB.js
+++ serverDB.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const username = 'user_mongo'
 const password = 'dobleq3'
 const dbname = 'electronica'
-const string_connection = `mongodb+srv://${username}:${password}@cluster0.sj5du.mongodb.net/${dbname}?retryWrites=true&w=majority`
+const string_connection = process.env.MONGODB_URI
 
 mongoose.connect(string_connection)
 .then(()=>{
